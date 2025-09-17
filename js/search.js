9f0f04b49fc7b8477d67145cdd2a4ba5be51456c
ccm89 <chris.colon.marq@gmail.com>
2025-09-17 19:50:51 -0400

    Starting fresh

--- js/search.js
+++ js/search.js
@@ -1,86 +1,4 @@
-// function searchContact()
-// {
-//     let srch = document.getElementById("searchText").value;
-//     document.getElementById("contactSearchResult").innerHTML = "";
-//     let contactList = "";
-
-//     let tmp = {search:srch, userId:userId};
-//     let jsonPayload = JSON.stringify(tmp);
-
-//     let url = urlBase + '/SearchContact.' + extension;
-    
-//     let xhr = new XMLHttpRequest();
-//     xhr.open("POST", url, true);
-//     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-//     try
-//     {
-//         xhr.onreadystatechange = function() 
-//         {
-//             if (this.readyState == 4 && this.status == 200) 
-//             {
-//                 let jsonObject = JSON.parse(xhr.responseText);
-                
-//                 if (jsonObject.error) {
-//                     document.getElementById("contactSearchResult").innerHTML = jsonObject.error;
-//                     document.getElementById("contactList").innerHTML = "";
-//                     return;
-//                 }
-
-//                 if(jsonObject.results.length === 0) {
-//                     document.getElementById("contactSearchResult").innerHTML = "No contacts found";
-//                     document.getElementById("contactList").innerHTML = "";
-//                 } else {
-//                     document.getElementById("contactSearchResult").innerHTML = "Contacts found";
-//                     // Create table header
-//                     let resultHTML = "<table border='1' style='width:100%; border-collapse: collapse;'>";
-//                     resultHTML += "<tr><th>Name</th><th>Phone</th><th>Email</th></tr>";
-                    
-//                     // Add each contact as a row
-//                     for(let i = 0; i < jsonObject.results.length; i++)
-//                     {
-//                         let contact = jsonObject.results[i];
-//                         resultHTML += "<tr>";
-//                         resultHTML += "<td>" + contact.firstName + " " + contact.lastName + "</td>";
-//                         resultHTML += "<td>" + contact.phone + "</td>";
-//                         resultHTML += "<td>" + contact.email + "</td>";
-//                         resultHTML += "</tr>";
-//                     }
-//                     resultHTML += "</table>";
-                    
-//                     // Display the results
-//                     document.getElementById("contactList").innerHTML = resultHTML;
-//                 }
-                
-//                 if (jsonObject.error) {
-//                     document.getElementById("contactSearchResult").innerHTML = jsonObject.error;
-//                     return;
-//                 }
-
-//                 // Create a table for better display
-//                 resultHTML = "<table border='1'><tr><th>Name</th><th>Phone</th><th>Email</th></tr>";
-                
-//                 for(let i = 0; i < jsonObject.results.length; i++)
-//                 {
-//                     let contact = jsonObject.results[i];
-//                     resultHTML += "<tr><td>" + contact.FirstName + " " + contact.LastName + 
-//                                 "</td><td>" + contact.Phone + 
-//                                 "</td><td>" + contact.Email + "</td></tr>";
-//                 }
-//                 resultHTML += "</table>";
-                
-//                 //document.getElementById("colorList").innerHTML = resultHTML;
-//                 document.getElementById("contactSearchResult").innerHTML = "Contacts retrieved";
-//             }
-//         };
-// 		xhr.send(jsonPayload);
-// 	}
-// 	catch(err)
-// 	{
-// 		document.getElementById("contactSearchResult").innerHTML = err.message;
-// 	}
-	
-// }
-
+// This file contains all functions used by the buttons in the search page: Search, Edit (Save/Cancel), Delete
 function searchContact() {
     let srch = document.getElementById("searchText").value;
     document.getElementById("contactSearchResult").innerHTML = "";
@@ -105,19 +23,21 @@ function searchContact() {
                 }
 
                 // Create table with Bootstrap classes
-                let resultHTML = "<table class='table table-striped table-hover'>";
-                resultHTML += "<thead class='table-primary'><tr><th>Name</th><th>Phone</th><th>Email</th><th>Actions</th></tr></thead>";
+                // NEW: Changed table to add borders
+                let resultHTML = "<table class='table table-striped table-bordered table-hover'>";
+                resultHTML += "<thead class='table-primary'><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Email</th><th>Actions</th></tr></thead>";
                 resultHTML += "<tbody>";
 
                 // Add each contact as a row
                 for (let i = 0; i < jsonObject.results.length; i++) {
                     let contact = jsonObject.results[i];
-                    resultHTML += "<tr>";
-                    resultHTML += "<td>" + contact.firstName + " " + contact.lastName + "</td>";
-                    resultHTML += "<td>" + contact.phone + "</td>";
-                    resultHTML += "<td>" + contact.email + "</td>";
-                    resultHTML += "<td><button class='btn btn-info btn-sm me-2' onclick='editContact(" + contact.id + ")'>Edit</button>" +
-                        "<button class='btn btn-danger btn-sm' onclick='deleteContact(" + contact.id + ")'>Delete</button></td>";
+                    resultHTML += "<tr id='row_" + i + "' data-id='" + contact.contactId + "'>";
+                    resultHTML += "<td id='firstName_" + i + "'>" + contact.firstName + "</td>";
+                    resultHTML += "<td id='lastName_" + i + "'>" + contact.lastName + "</td>";
+                    resultHTML += "<td id='phone_" + i + "'>" + contact.phone + "</td>";
+                    resultHTML += "<td id='email_" + i + "'>" + contact.email + "</td>";
+                    resultHTML += "<td><button class='btn btn-info btn-sm me-2' onclick='editContact(" + i + "," + contact.contactId + ")'><i class='bi bi-pencil-square'></i></button>" +
+                        "<button class='btn btn-danger btn-sm' onclick='deleteContact(" + contact.contactId + ")'><i class='bi bi-trash3'></i></button></td>";
                     resultHTML += "</tr>";
                 }
                 resultHTML += "</tbody></table>";
@@ -136,17 +56,134 @@ function searchContact() {
 
 }
 
-// ==================================TODO======================================
-function editContact(id) {
-    // Get the contact details from your server
-    // Show a form/modal with current values
-    // Update the contact when form is submitted
+// NEW: edit function to add functionality to edit contact button
+function editContact(rowIndex, contactId) {
+    // Get current row values
+    let cellFName = document.getElementById("firstName_" + rowIndex);
+    let cellLName = document.getElementById("lastName_" + rowIndex);
+    let cellPhone = document.getElementById("phone_" + rowIndex);
+    let cellEmail = document.getElementById("email_" + rowIndex);
+
+    // Getting rid of extra white space
+    let fName = cellFName.textContent.trim();
+    let lName = cellLName.textContent.trim();
+    let phone = cellPhone.textContent.trim();
+    let email = cellEmail.textContent.trim();
+
+    // Cell become inputs
+    cellFName.innerHTML = "<input class='form-control' id='edited_fName_" + rowIndex + "'value='" + fName + "'>";
+    cellLName.innerHTML = "<input class='form-control' id='edited_lName_" + rowIndex + "'value='" + lName + "'>";
+    cellPhone.innerHTML = "<input class='form-control' id='edited_phone_" + rowIndex + "'value='" + phone + "'>";
+    cellEmail.innerHTML = "<input class='form-control' id='edited_email_" + rowIndex + "'value='" + email + "'>";
+
+    // Action buttons Save/Cancel
+    let actionOnTd = cellFName.parentElement.querySelector("td:last-child");
+    actionOnTd.innerHTML = "<button class='btn btn-success' onclick='saveContact(" + rowIndex + "," + contactId + ")'>Save</button>" + "<button class='btn btn-secondary' onclick='cancelEdit(" + rowIndex + "," + JSON.stringify({fName,lName,phone,email}) + ")'>Cancel</button>"
+
 }
 
-function deleteContact(id) {
-    // Show confirmation dialog
-    if(confirm("Are you sure you want to delete this contact?")) {
-        // Send delete request to your server
-        // If successful, refresh the search results
+// NEW: function to cancel the edit action
+function cancelEdit(rowIndex, original_values) {
+    // Restore cells to original values
+    document.getElementById("firstName_" + rowIndex).textContent = original_values.fName;
+    document.getElementById("lastName_" + rowIndex).textContent = original_values.lName;
+    document.getElementById("phone_" + rowIndex).textContent = original_values.phone;
+    document.getElementById("email_" + rowIndex).textContent = original_values.email;
+
+    // Restores buttons back to Edit/Delete
+    searchContact();
+}
+
+// New: function to save the edited cells
+function saveContact(rowIndex, contactId) {
+    document.getElementById("contactDeleteResult").innerHTML = "";
+
+    // Gather edits
+    let firstName = document.getElementById("edited_fName_" + rowIndex).value.trim();
+    let lastName = document.getElementById("edited_lName_" + rowIndex).value.trim();
+    let phone = document.getElementById("edited_phone_" + rowIndex).value.trim();
+    let email = document.getElementById("edited_email_" + rowIndex).value.trim();
+    
+    // Minimum requirement check 
+    if (!firstName || !lastName) {
+        document.getElementById("contactDeleteResult").innerHTML = "First and Last name required!"
+    }
+    let payload = {
+        contactId: Number(contactId),
+        userId: Number(userId),
+        firstName,
+        lastName,
+        phone,
+        email
+    };
+
+    let url = urlBase + "/EditContact." + extension;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if(this.readyState === 4) {
+            let resp = {};
+            try { resp = JSON.parse(this.responseText); } catch {}
+            if (this.status !== 200 || resp.error) {
+                document.getElementById("contactDeleteResult").innerHTML = resp.error || "Update failed.";
+                return;
+            }
+            // Success --> reload contact list with updated data
+            searchContact();
+        }
+    };
+    xhr.send(JSON.stringify(payload))
+}
+
+// NEW: delete function to add functionality to delete contact button
+function deleteContact(contactId) {
+    // Clear previous messages
+    document.getElementById("contactDeleteResult").innerHTML = "";
+
+    let idNum = Number(contactId);
+    // Validate contact to delete
+    if (!Number.isInteger(idNum) || idNum <= 0) {
+        document.getElementById("contactDeleteResult").innerHTML = "Invalid contact!";
+        return;
+    }
+
+    // TODO: To be changed to modal component
+    if (!confirm("You are about to delete this contact! Confirm by clicking okay.")) {
+        return;
+    }
+
+    let tmp = {
+        contactId: idNum,
+        userId: userId
+    };
+
+    let jsonPayload = JSON.stringify(tmp);
+
+    let url = urlBase + '/DeleteContact.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+            xhr.onreadystatechange = function () {
+
+                if (this.readyState == 4 && this.status == 200) {
+                    let response = JSON.parse(xhr.responseText);
+                    if (response.error) {
+                        document.getElementById("contactDeleteResult").innerHTML = response.error;
+                        return;
+                    }
+                    document.getElementById("contactDeleteResult").innerHTML = "Contact deleted successfully";
+
+                    // Refresh the contact list
+                    searchContact();
+                }
+            };
+            xhr.send(jsonPayload);
+    }
+    catch (err) {
+        document.getElementById("contactDeleteResult").innerHTML = err.message;
     }
 }
