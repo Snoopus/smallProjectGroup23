1fdbd2c3e97a188ccc33dcba53b2454b00a1896a
chris <chris.colon.marq@gmail.com>
2025-09-23 12:20:01 -0400

    Added conditional to check password and re-entered password are same.

--- js/register.js
+++ js/register.js
@@ -1,5 +1,3 @@
-
-
 function doRegister()
 {
     // Get the input values
@@ -7,6 +5,7 @@ function doRegister()
     lastName = document.getElementById("userLastName").value;
     let login = document.getElementById("userName").value;
     let password = document.getElementById("userPassword").value;
+    let confirm_password = document.getElementById("userPassword-repeat").value;
 
     // Clear any previous error messages
     document.getElementById("registerResult").innerHTML = "";
@@ -14,6 +13,21 @@ function doRegister()
     // Validate inputs
     if (!firstName || !lastName || !login || !password) {
         document.getElementById("registerResult").innerHTML = "Please fill in all fields";
+        return;
+    }
+
+    // Validate passwords
+    if (password !== confirm_password) {
+        document.getElementById("registerResult").innerHTML = "Wow buddy slow down the passwords do not match!";
+        
+        // Warn user of mismatch
+        let password_warning = document.getElementById("userPassword");
+        let conf_pass_warning = document.getElementById("userPassword-repeat");
+        password_warning.setCustomValidity("Passwords do not match");
+        conf_pass_warning.setCustomValidity("Passwords do not match");
+        
+        // Add warning style back
+        document.querySelector('.need-validation').classList.add('was-validated');
         return;
     }
 
