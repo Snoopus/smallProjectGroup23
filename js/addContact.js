f691368647b215f4b530574ec459a6e4f7f4a39d
chris <chris.colon.marq@gmail.com>
2025-09-24 14:53:56 -0400

    Added conditional to show success message and clears in 1 second.

--- js/addContact.js
+++ js/addContact.js
@@ -1,13 +1,12 @@
-function addContact()
-{
+function addContact() {
     let firstName = document.getElementById("contactTextFirstName").value;
     let lastName = document.getElementById("contactTextLastName").value;
     let phone = document.getElementById("contactTextPhone").value;
     let email = document.getElementById("contactTextEmail").value;
-    
+
     // Clear previous messages
     document.getElementById("contactAddResult").innerHTML = "";
-    
+
     // Validate inputs
     if (!firstName || !lastName) {
         document.getElementById("contactAddResult").innerHTML = "Please fill in all fields";
@@ -24,23 +23,32 @@ function addContact()
     let jsonPayload = JSON.stringify(tmp);
 
     let url = urlBase + '/AddContact.' + extension;
-    
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-    try
-    {
-        xhr.onreadystatechange = function() 
-        {
-            if (this.readyState == 4 && this.status == 200) 
-            {
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
                 let response = JSON.parse(xhr.responseText);
                 if (response.error) {
                     document.getElementById("contactAddResult").innerHTML = response.error;
                     return;
                 }
-                document.getElementById("contactAddResult").innerHTML = "Contact added successfully";
-                
+                // Modify and display success message w/ green color. 
+                let res = document.getElementById("contactAddResult");
+                if (res) {
+                    res.innerHTML = "Contact added successfully";
+                    res.classList.remove('text-danger');
+                    res.classList.add('text-success');
+
+                    // Clear success message in 1 seconds.
+                    setTimeout(() => {
+                        res.innerHTML = "";
+                        res.className = "mt-3 fw-bold text-center text-danger";
+                    }, 1000);
+                }
+
                 // Clear the input fields
                 document.getElementById("contactTextFirstName").value = "";
                 document.getElementById("contactTextLastName").value = "";
@@ -49,15 +57,12 @@ function addContact()
 
                 // Clear validation
                 document.querySelector('.needs-validation').classList.remove('was-validated');
-                
-                // Refresh the contact list
-                searchContact();
+
             }
         };
         xhr.send(jsonPayload);
     }
-    catch(err)
-    {
+    catch (err) {
         document.getElementById("contactAddResult").innerHTML = err.message;
     }
 }
